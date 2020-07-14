8a806748a43f204fee001567c2a67cc76f04720f
Виктор Маркозанов <viktor.markozanov@students.finki.ukim.mk>
2020-07-14 21:24:24 +0200

    Docker

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from  'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://backendcontainer:8080',
     headers : {
         'Access-Control-Allow-Origin': '*'
     },
